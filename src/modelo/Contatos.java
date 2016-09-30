166e4f0344436a2fbec218b1af084fc3822384f6
aluno <aluno@L516>
2016-09-30 20:45:21 -0300

    Versão 30-09-2016 commit 2

--- src/modelo/Contatos.java
+++ src/modelo/Contatos.java
@@ -1,12 +1,11 @@
 /*
- * -------------------------------------------------
- * alphaDev - Desenvolvimento de Projetos Sistêmicos
- * -------------------------------------------------
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package modelo;
 
 import java.io.Serializable;
-import java.math.BigDecimal;
 import java.util.Date;
 import javax.persistence.Basic;
 import javax.persistence.Column;
@@ -23,150 +22,156 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  *
- * @author fernando
+ * @author aluno
  */
 @Entity
 @Table(catalog = "db_fatec", schema = "")
 @XmlRootElement
 @NamedQueries({
-  @NamedQuery(name = "Contatos.findAll", query = "SELECT c FROM Contatos c"),
-  @NamedQuery(name = "Contatos.findById", query = "SELECT c FROM Contatos c WHERE c.id = :id"),
-  @NamedQuery(name = "Contatos.findByNome", query = "SELECT c FROM Contatos c WHERE c.nome = :nome"),
-  @NamedQuery(name = "Contatos.findBySexo", query = "SELECT c FROM Contatos c WHERE c.sexo = :sexo"),
-  @NamedQuery(name = "Contatos.findByFoneRes", query = "SELECT c FROM Contatos c WHERE c.foneRes = :foneRes"),
-  @NamedQuery(name = "Contatos.findByFoneCel", query = "SELECT c FROM Contatos c WHERE c.foneCel = :foneCel"),
-  @NamedQuery(name = "Contatos.findByEmail", query = "SELECT c FROM Contatos c WHERE c.email = :email"),
-  @NamedQuery(name = "Contatos.findByDataNas", query = "SELECT c FROM Contatos c WHERE c.dataNas = :dataNas"),
-  @NamedQuery(name = "Contatos.findBySalario", query = "SELECT c FROM Contatos c WHERE c.salario = :salario")})
+    @NamedQuery(name = "Contatos.findAll", query = "SELECT c FROM Contatos c"),
+    @NamedQuery(name = "Contatos.findById", query = "SELECT c FROM Contatos c WHERE c.id = :id"),
+    @NamedQuery(name = "Contatos.findByNome", query = "SELECT c FROM Contatos c WHERE c.nome = :nome"),
+    @NamedQuery(name = "Contatos.findBySexo", query = "SELECT c FROM Contatos c WHERE c.sexo = :sexo"),
+    @NamedQuery(name = "Contatos.findByFoneRes", query = "SELECT c FROM Contatos c WHERE c.foneRes = :foneRes"),
+    @NamedQuery(name = "Contatos.findByFoneCel", query = "SELECT c FROM Contatos c WHERE c.foneCel = :foneCel"),
+    @NamedQuery(name = "Contatos.findByEmail", query = "SELECT c FROM Contatos c WHERE c.email = :email"),
+    @NamedQuery(name = "Contatos.findByDataNas", query = "SELECT c FROM Contatos c WHERE c.dataNas = :dataNas"),
+    @NamedQuery(name = "Contatos.findBySalario", query = "SELECT c FROM Contatos c WHERE c.salario = :salario")})
 public class Contatos implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(nullable = false)
+    private Integer id;
+    @Basic(optional = false)
+    @Column(nullable = false, length = 50)
+    private String nome;
+    @Basic(optional = false)
+    @Column(nullable = false)
+    private Character sexo;
+    @Basic(optional = false)
+    @Column(name = "fone_res", nullable = false, length = 15)
+    private String foneRes;
+    @Basic(optional = false)
+    @Column(name = "fone_cel", nullable = false, length = 15)
+    private String foneCel;
+    @Basic(optional = false)
+    @Column(nullable = false, length = 50)
+    private String email;
+    @Basic(optional = false)
+    @Column(name = "data_nas", nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date dataNas;
+    @Basic(optional = false)
+    @Column(nullable = false)
+    private float salario;
 
-  private static final long serialVersionUID = 1L;
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Basic(optional = false)
-  @Column(nullable = false)
-  private Integer id;
-  @Basic(optional = false)
-  @Column(nullable = false, length = 40)
-  private String nome;
-  @Basic(optional = false)
-  @Column(nullable = false, length = 1)
-  private String sexo;
-  @Column(name = "fone_res", length = 15)
-  private String foneRes;
-  @Column(name = "fone_cel", length = 15)
-  private String foneCel;
-  @Basic(optional = false)
-  @Column(nullable = false, length = 50)
-  private String email;
-  @Column(name = "data_nas")
-  @Temporal(TemporalType.TIMESTAMP)
-  private Date dataNas;
-  // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
-  @Column(precision = 10, scale = 2)
-  private BigDecimal salario;
-
-  public Contatos() {
-  }
-
-  public Contatos( Integer id ) {
-    this.id = id;
-  }
-
-  public Contatos( Integer id, String nome, String sexo, String email ) {
-    this.id = id;
-    this.nome = nome;
-    this.sexo = sexo;
-    this.email = email;
-  }
-
-  public Integer getId() {
-    return id;
-  }
-
-  public void setId( Integer id ) {
-    this.id = id;
-  }
-
-  public String getNome() {
-    return nome;
-  }
-
-  public void setNome( String nome ) {
-    this.nome = nome;
-  }
-
-  public String getSexo() {
-    return sexo;
-  }
-
-  public void setSexo( String sexo ) {
-    this.sexo = sexo;
-  }
-
-  public String getFoneRes() {
-    return foneRes;
-  }
-
-  public void setFoneRes( String foneRes ) {
-    this.foneRes = foneRes;
-  }
-
-  public String getFoneCel() {
-    return foneCel;
-  }
-
-  public void setFoneCel( String foneCel ) {
-    this.foneCel = foneCel;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail( String email ) {
-    this.email = email;
-  }
-
-  public Date getDataNas() {
-    return dataNas;
-  }
-
-  public void setDataNas( Date dataNas ) {
-    this.dataNas = dataNas;
-  }
-
-  public BigDecimal getSalario() {
-    return salario;
-  }
-
-  public void setSalario( BigDecimal salario ) {
-    this.salario = salario;
-  }
-
-  @Override
-  public int hashCode() {
-    int hash = 0;
-    hash += (id != null ? id.hashCode() : 0);
-    return hash;
-  }
-
-  @Override
-  public boolean equals( Object object ) {
-    // TODO: Warning - this method won't work in the case the id fields are not set
-    if ( !(object instanceof Contatos) ) {
-      return false;
+    public Contatos() {
     }
-    Contatos other = (Contatos) object;
-    if ( (this.id == null && other.id != null) || (this.id != null && !this.id.equals( other.id )) ) {
-      return false;
+
+    public Contatos(Integer id) {
+        this.id = id;
     }
-    return true;
-  }
 
-  @Override
-  public String toString() {
-    return "modelo.Contatos[ id=" + id + " ]";
-  }
+    public Contatos(Integer id, String nome, Character sexo, String foneRes, String foneCel, String email, Date dataNas, float salario) {
+        this.id = id;
+        this.nome = nome;
+        this.sexo = sexo;
+        this.foneRes = foneRes;
+        this.foneCel = foneCel;
+        this.email = email;
+        this.dataNas = dataNas;
+        this.salario = salario;
+    }
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Character getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(Character sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getFoneRes() {
+        return foneRes;
+    }
+
+    public void setFoneRes(String foneRes) {
+        this.foneRes = foneRes;
+    }
+
+    public String getFoneCel() {
+        return foneCel;
+    }
+
+    public void setFoneCel(String foneCel) {
+        this.foneCel = foneCel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDataNas() {
+        return dataNas;
+    }
+
+    public void setDataNas(Date dataNas) {
+        this.dataNas = dataNas;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Contatos)) {
+            return false;
+        }
+        Contatos other = (Contatos) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "modelo.Contatos[ id=" + id + " ]";
+    }
+    
 }
--- src/modelo/Usuarios.java
+++ src/modelo/Usuarios.java
@@ -1,7 +1,7 @@
 /*
- * -------------------------------------------------
- * alphaDev - Desenvolvimento de Projetos Sistêmicos
- * -------------------------------------------------
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package modelo;
 
@@ -22,191 +22,175 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  *
- * @author fernando
+ * @author aluno
  */
 @Entity
 @Table(catalog = "db_fatec", schema = "")
 @XmlRootElement
 @NamedQueries({
-  @NamedQuery(name = "Usuarios.findAll", query = "SELECT u FROM Usuarios u"),
-  @NamedQuery(name = "Usuarios.findById", query = "SELECT u FROM Usuarios u WHERE u.id = :id"),
-  @NamedQuery(name = "Usuarios.findByNome", query = "SELECT u FROM Usuarios u WHERE u.nome = :nome"),
-  @NamedQuery(name = "Usuarios.findByNivel", query = "SELECT u FROM Usuarios u WHERE u.nivel = :nivel"),
-  @NamedQuery(name = "Usuarios.findByUsuario", query = "SELECT u FROM Usuarios u WHERE u.usuario = :usuario"),
-  @NamedQuery(name = "Usuarios.findBySenha", query = "SELECT u FROM Usuarios u WHERE u.senha = :senha"),
-  @NamedQuery(name = "Usuarios.findByEmail", query = "SELECT u FROM Usuarios u WHERE u.email = :email"),
-  @NamedQuery(name = "Usuarios.findByTema", query = "SELECT u FROM Usuarios u WHERE u.tema = :tema"),
-  @NamedQuery(name = "Usuarios.findByCreateAt", query = "SELECT u FROM Usuarios u WHERE u.createAt = :createAt"),
-  @NamedQuery(name = "Usuarios.findByUpdateAt", query = "SELECT u FROM Usuarios u WHERE u.updateAt = :updateAt"),
-  @NamedQuery(name = "Usuarios.findByDeleteAt", query = "SELECT u FROM Usuarios u WHERE u.deleteAt = :deleteAt"),
-  @NamedQuery(name = "Usuarios.findByUsuarioESenha", query = "SELECT u FROM Usuarios u WHERE u.usuario = :usuario AND u.senha = :senha")})
-
+    @NamedQuery(name = "Usuarios.findAll", query = "SELECT u FROM Usuarios u"),
+    @NamedQuery(name = "Usuarios.findById", query = "SELECT u FROM Usuarios u WHERE u.id = :id"),
+    @NamedQuery(name = "Usuarios.findByNome", query = "SELECT u FROM Usuarios u WHERE u.nome = :nome"),
+    @NamedQuery(name = "Usuarios.findByUsuario", query = "SELECT u FROM Usuarios u WHERE u.usuario = :usuario"),
+    @NamedQuery(name = "Usuarios.findBySenha", query = "SELECT u FROM Usuarios u WHERE u.senha = :senha"),
+    @NamedQuery(name = "Usuarios.findByNivel", query = "SELECT u FROM Usuarios u WHERE u.nivel = :nivel"),
+    @NamedQuery(name = "Usuarios.findByTema", query = "SELECT u FROM Usuarios u WHERE u.tema = :tema"),
+    @NamedQuery(name = "Usuarios.findByEmail", query = "SELECT u FROM Usuarios u WHERE u.email = :email"),
+    @NamedQuery(name = "Usuarios.findByCreateAt", query = "SELECT u FROM Usuarios u WHERE u.createAt = :createAt"),
+    @NamedQuery(name = "Usuarios.findByUpdateAt", query = "SELECT u FROM Usuarios u WHERE u.updateAt = :updateAt"),
+    @NamedQuery(name = "Usuarios.findByDeleteAt", query = "SELECT u FROM Usuarios u WHERE u.deleteAt = :deleteAt"),
+    @NamedQuery(name = "Usuarios.findByUsuarioAndSenha", query = "SELECT u FROM Usuarios u WHERE u.usuario = :usuario AND u.senha = :senha")})
 public class Usuarios implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(nullable = false)
+    private Integer id;
+    @Basic(optional = false)
+    @Column(nullable = false, length = 40)
+    private String nome;
+    @Basic(optional = false)
+    @Column(nullable = false, length = 15)
+    private String usuario;
+    @Basic(optional = false)
+    @Column(nullable = false, length = 15)
+    private String senha;
+    @Basic(optional = false)
+    @Column(nullable = false, length = 15)
+    private String nivel;
+    @Column(length = 50)
+    private String tema;
+    @Column(length = 50)
+    private String email;
+    @Column(name = "create_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createAt;
+    @Column(name = "update_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updateAt;
+    @Column(name = "delete_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date deleteAt;
 
-  private static final long serialVersionUID = 1L;
-
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Basic(optional = false)
-  @Column(nullable = false)
-  private Integer id;
-
-  @Basic(optional = false)
-  @Column(nullable = false, length = 40)
-  private String nome;
-
-  @Basic(optional = false)
-  @Column(nullable = false, length = 20)
-  private String nivel;
-
-  @Basic(optional = false)
-  @Column(nullable = false, length = 20)
-  private String usuario;
-
-  @Basic(optional = false)
-  @Column(nullable = false, length = 15)
-  private String senha;
-
-  @Basic(optional = false)
-  @Column(nullable = false, length = 50)
-  private String email;
-
-  @Basic(optional = false)
-  @Column(nullable = false, length = 50)
-  private String tema;
-
-  @Column(name = "create_at")
-  @Temporal(TemporalType.TIMESTAMP)
-  private Date createAt;
-
-  @Column(name = "update_at")
-  @Temporal(TemporalType.TIMESTAMP)
-  private Date updateAt;
-
-  @Column(name = "delete_at")
-  @Temporal(TemporalType.TIMESTAMP)
-  private Date deleteAt;
-
-  public Usuarios() {
-  }
-
-  public Usuarios( Integer id ) {
-    this.id = id;
-  }
-
-  public Usuarios( Integer id, String nome, String nivel, String usuario, String senha, String email, String tema ) {
-    this.id = id;
-    this.nome = nome;
-    this.nivel = nivel;
-    this.usuario = usuario;
-    this.senha = senha;
-    this.email = email;
-    this.tema = tema;
-  }
-
-  public Integer getId() {
-    return id;
-  }
-
-  public void setId( Integer id ) {
-    this.id = id;
-  }
-
-  public String getNome() {
-    return nome;
-  }
-
-  public void setNome( String nome ) {
-    this.nome = nome;
-  }
-
-  public String getNivel() {
-    return nivel;
-  }
-
-  public void setNivel( String nivel ) {
-    this.nivel = nivel;
-  }
-
-  public String getUsuario() {
-    return usuario;
-  }
-
-  public void setUsuario( String usuario ) {
-    this.usuario = usuario;
-  }
-
-  public String getSenha() {
-    return senha;
-  }
-
-  public void setSenha( String senha ) {
-    this.senha = senha;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail( String email ) {
-    this.email = email;
-  }
-
-  public String getTema() {
-    return tema;
-  }
-
-  public void setTema( String tema ) {
-    this.tema = tema;
-  }
-
-  public Date getCreateAt() {
-    return createAt;
-  }
-
-  public void setCreateAt( Date createAt ) {
-    this.createAt = createAt;
-  }
-
-  public Date getUpdateAt() {
-    return updateAt;
-  }
-
-  public void setUpdateAt( Date updateAt ) {
-    this.updateAt = updateAt;
-  }
-
-  public Date getDeleteAt() {
-    return deleteAt;
-  }
-
-  public void setDeleteAt( Date deleteAt ) {
-    this.deleteAt = deleteAt;
-  }
-
-  @Override
-  public int hashCode() {
-    int hash = 0;
-    hash += (id != null ? id.hashCode() : 0);
-    return hash;
-  }
-
-  @Override
-  public boolean equals( Object object ) {
-    // TODO: Warning - this method won't work in the case the id fields are not set
-    if ( !(object instanceof Usuarios) ) {
-      return false;
+    public Usuarios() {
     }
-    Usuarios other = (Usuarios) object;
-    if ( (this.id == null && other.id != null) || (this.id != null && !this.id.equals( other.id )) ) {
-      return false;
+
+    public Usuarios(Integer id) {
+        this.id = id;
     }
-    return true;
-  }
 
-  @Override
-  public String toString() {
-    return "modelo.Usuarios[ id=" + id + " ]";
-  }
+    public Usuarios(Integer id, String nome, String usuario, String senha, String nivel) {
+        this.id = id;
+        this.nome = nome;
+        this.usuario = usuario;
+        this.senha = senha;
+        this.nivel = nivel;
+    }
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(String nivel) {
+        this.nivel = nivel;
+    }
+
+    public String getTema() {
+        return tema;
+    }
+
+    public void setTema(String tema) {
+        this.tema = tema;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public Date getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Date updateAt) {
+        this.updateAt = updateAt;
+    }
+
+    public Date getDeleteAt() {
+        return deleteAt;
+    }
+
+    public void setDeleteAt(Date deleteAt) {
+        this.deleteAt = deleteAt;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Usuarios)) {
+            return false;
+        }
+        Usuarios other = (Usuarios) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "modelo.Usuarios[ id=" + id + " ]";
+    }
+    
 }
